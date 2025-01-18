b3ceb4eb37e771c2bd6b67c7f8c138b9b4d5b238
Blindeen <kuba.szmajda@gmail.com>
2025-01-18 19:17:56 +0100

    add handler to create a note

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,14 +1,26 @@
 import { useRef, useState } from 'react';
 
+import { Note as NoteComponent } from '../Note';
 import { Note } from '../../model/note.model';
+import { drawCoordinates } from '../../utils/drawCoordinates';
 import styles from './board.module.css';
 
 const Board = () => {
     const boardRef = useRef<HTMLDivElement>(null);
     const [notes, setNotes] = useState<Note[]>([]);
 
-    const addNote = () => {
-        console.log('Note added!');
+    const addNoteHandler = () => {
+        const { clientHeight, clientWidth } = boardRef.current!;
+        const newCoordinates = drawCoordinates(
+            clientWidth - 114.6,
+            clientHeight - 114.6
+        );
+        const newNote: Note = {
+            id: notes.length + 1,
+            text: '',
+            position: newCoordinates,
+        };
+        setNotes((prevNotes) => [...prevNotes, newNote]);
     };
 
     return (
@@ -16,7 +28,7 @@ const Board = () => {
             <button
                 type='button'
                 className={styles.addButton}
-                onClick={addNote}
+                onClick={addNoteHandler}
             >
                 +
             </button>
@@ -24,6 +36,9 @@ const Board = () => {
                 <span className={styles.logoText}>Sticky Board</span>
                 <span className={styles.logoNote}></span>
             </div>
+            {notes.map(({ id, text, position }) => (
+                <NoteComponent key={id} text={text} position={position} />
+            ))}
         </div>
     );
 };
---
+++ src/utils/drawCoordinates.ts
@@ -0,0 +1,11 @@
+const drawCoordinates = (maxX: number, maxY: number) => {
+    let x = Math.floor(Math.random() * (maxX + 1));
+    x = Math.min(Math.max(0, x), maxX);
+
+    let y = Math.floor(Math.random() * (maxY + 1));
+    y = Math.min(Math.max(0, y), maxY);
+
+    return { x, y };
+};
+
+export { drawCoordinates };
