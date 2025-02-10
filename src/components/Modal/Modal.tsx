206e1cb7cd4de446f6753bd2e3b84c0921d2ac5f
Blindeen <kuba.szmajda@gmail.com>
2025-02-10 20:28:30 +0100

    style, add logic and use component

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,27 +1,61 @@
+import { useRef, useEffect, KeyboardEvent } from 'react';
+
 import styles from './modal.module.css';
 
 interface ModalProps {
-    isOpen: boolean;
     setIsOpen: (value: boolean) => void;
+    onOk: () => void;
+    isOpen: boolean;
 }
 
-const Modal = ({ isOpen, setIsOpen }: ModalProps) => {
+const Modal = ({ setIsOpen, onOk, isOpen }: ModalProps) => {
+    const dialogRef = useRef<HTMLDialogElement>(null);
+
+    useEffect(() => {
+        if (isOpen) {
+            dialogRef.current?.showModal();
+        } else {
+            dialogRef.current?.close();
+        }
+    }, [isOpen]);
+
     const onClose = () => setIsOpen(false);
 
+    const onKeyDown = (e: KeyboardEvent<HTMLDialogElement>) => {
+        if (e.key === 'Escape') {
+            onClose();
+        }
+    };
+
+    const onDelete = () => {
+        onOk();
+        onClose();
+    };
+
     return (
-        <dialog className={styles.dialog} open={isOpen}>
-            <header>
-                <h2>Delete the task</h2>
-            </header>
-            <p>Are you sure, you want to delete the task?</p>
-            <footer className={styles.footer}>
-                <button type='button' onClick={onClose}>
-                    Cancel
-                </button>
-                <button type='button' onClick={onClose}>
-                    Delete
-                </button>
-            </footer>
+        <dialog className={styles.dialog} ref={dialogRef} onKeyDown={onKeyDown}>
+            <div className={styles.content}>
+                <header>
+                    <h2 className={styles.h2}>Delete tasks</h2>
+                </header>
+                <main>Are you sure, you want to clear the board?</main>
+                <footer className={styles.footer}>
+                    <button
+                        className={styles.button}
+                        type='button'
+                        onClick={onClose}
+                    >
+                        Cancel
+                    </button>
+                    <button
+                        className={`${styles.button} ${styles.deleteButton}`}
+                        type='button'
+                        onClick={onDelete}
+                    >
+                        Delete
+                    </button>
+                </footer>
+            </div>
         </dialog>
     );
 };
