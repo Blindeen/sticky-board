400c6fe628c8bbbf2f7fb7f32a404176b384f743
Blindeen <kuba.szmajda@gmail.com>
2025-03-03 19:50:38 +0100

    add zustand's persist middleware

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 import { Note } from './model/note.model';
 import { NoteData } from './model/note-data.model';
@@ -13,47 +14,56 @@ interface NoteState {
     deleteNotes: () => void;
 }
 
-const useStore = create<NoteState>((set) => ({
-    notes: [],
-    addNote: () => {
-        const newNote: Note = {
-            id: Date.now(),
-            title: '',
-            description: '',
-            leftCornerCoords: { x: 5, y: 5 },
-        };
-        set((state) => ({ notes: [...state.notes, newNote] }));
-    },
-    updateNote: (id, data) => {
-        set((state) => {
-            const newNotes = [...state.notes];
-            const note = newNotes.find((note) => note.id === id);
-            if (note) {
-                note.title = data.title;
-                note.description = data.description;
-            }
-            return { notes: newNotes };
-        });
-    },
-    moveNote: (id, coords) => {
-        set((state) => {
-            const newNotes = [...state.notes];
-            const note = newNotes.find((note) => note.id === id);
-            if (note) {
-                note.leftCornerCoords = coords;
-            }
-            return { notes: newNotes };
-        });
-    },
-    deleteNote: (id) => {
-        set((state) => {
-            const newNotes = state.notes.filter((note) => note.id !== id);
-            return { notes: newNotes };
-        });
-    },
-    deleteNotes: () => {
-        set(() => ({ notes: [] }));
-    },
-}));
+const useStore = create<NoteState>()(
+    persist(
+        (set) => ({
+            notes: [],
+            addNote: () => {
+                const newNote: Note = {
+                    id: Date.now(),
+                    title: '',
+                    description: '',
+                    leftCornerCoords: { x: 5, y: 5 },
+                };
+                set((state) => ({ notes: [...state.notes, newNote] }));
+            },
+            updateNote: (id, data) => {
+                set((state) => {
+                    const newNotes = [...state.notes];
+                    const note = newNotes.find((note) => note.id === id);
+                    if (note) {
+                        note.title = data.title;
+                        note.description = data.description;
+                    }
+                    return { notes: newNotes };
+                });
+            },
+            moveNote: (id, coords) => {
+                set((state) => {
+                    const newNotes = [...state.notes];
+                    const note = newNotes.find((note) => note.id === id);
+                    if (note) {
+                        note.leftCornerCoords = coords;
+                    }
+                    return { notes: newNotes };
+                });
+            },
+            deleteNote: (id) => {
+                set((state) => {
+                    const newNotes = state.notes.filter(
+                        (note) => note.id !== id
+                    );
+                    return { notes: newNotes };
+                });
+            },
+            deleteNotes: () => {
+                set(() => ({ notes: [] }));
+            },
+        }),
+        {
+            name: 'notes-storage',
+        }
+    )
+);
 
 export { useStore };
